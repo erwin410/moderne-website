2596bcc86c451d92c1de4f9ebeacbc52d14b0ac9
lvsprod <erwinpateux@gmail.com>
2022-07-05 18:03:11 +0400

    File & Folder Structure (setup)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
+import { CTA, Brand, Navbar } from './components'
+
+import './App.css'
+
 function App() {
   return (
-    <div>
-        <h1> Moderne Website UI/UX </h1>
+    <div className='App'>
+       <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+       </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
