8bd567836fe109fc149f4309a9d1b44f85696d4b
lvsprod <erwinpateux@gmail.com>
2022-07-06 12:20:32 +0400

    Header (Styling Setup)

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './header.css'
 
 import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 
 function Header() {
   return (
     <div className='gpt3__header section__padding' id='home'>
             <div className='gpt3__header-content'>
-              <h1 className="gradient__text">Let's Build Something amazing whit GPT3 OpenAI</h1>
+              <h1 className='gradient__text'>Let's Build Something amazing whit GPT3 OpenAI</h1>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Nesciunt soluta omnis laboriosam hic voluptatem obcaecati quae fugit!
                  Quasi iste aut dolores, itaque nostrum voluptate consectetur, cum quia porro, eveniet sed.</p>
@@ -19,9 +20,13 @@ function Header() {
               </div>
 
               <div className='gpt3__header-content__people'>
-                <img src={people} alt="" />
+                  <img src={people} alt="people" />
+                  <p>1,600 people requested access a visit in last 24 hours</p>
+               </div>
+              <div className='gpt3__header-image'>
+                <img src={ai} alt="ai" />
               </div>
-            </div>
+          </div>
     </div>
   )
 }
